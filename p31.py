868738b6556087d620a767e2b1f4e2a0faae3a96
Neda Jalili <neda.jalili.23@gmail.com>
2024-10-18 21:32:43 +0330

    Add files via upload

---
+++ p31.py
@@ -0,0 +1,22 @@
+import pandas as pd
+data=pd.read_excel("salary.xlsx")
+x=data.iloc[:,:-1].values
+y=data.iloc[:,1].values
+
+from sklearn.linear_model import LinearRegression
+model=LinearRegression()
+model.fit(x,y)
+ypred=model.predict(x)
+y10=model.predict([[10]])
+print("y10 = ",y10)
+
+from sklearn.metrics import r2_score
+print("r2 = ",r2_score(y,ypred))
+
+import matplotlib.pyplot as plt
+plt.scatter(x,y,label="data")
+plt.plot(x,ypred,label="fit")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend()
+plt.show()
